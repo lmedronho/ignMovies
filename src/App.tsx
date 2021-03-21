40559e9d5dcd9adb8a5da162d2df75dc35c524e7
Luiz Medronho <lmedronho@gmail.com>
2021-03-21 20:29:39 -0300

    :tada: Challenge coded

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+
+import { Button } from './components/Button';
+import { MovieCard } from './components/MovieCard';
+
+// import { SideBar } from './components/SideBar';
+// import { Content } from './components/Content';
+
+import { api } from './services/api';
+
+import './styles/global.scss';
+
+import './styles/sidebar.scss';
+import './styles/content.scss';
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
+import { MoviesProvider } from './MoviesContext';
+
+
+
+export function App() {
+  
+
+  return (
+
+    <MoviesProvider>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+           
+        <SideBar />
+
+        <Content  />
+
+      </div>
+
+    </MoviesProvider>
+    
+  )
+}
